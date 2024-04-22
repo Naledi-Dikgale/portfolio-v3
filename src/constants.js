25ef4de957e2a91e227c2d2b937de05f614c7b1e
naledi-dikgale <naledi22khosi@gmail.com>
2024-04-22 20:40:30 +0200

    Add constants

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const FAQ_DATA = [
+  {
+    question: 'What is software engineering?',
+    answer: 'Software engineering is the process of analyzing user needs and designing, constructing, and testing end user applications that will satisfy these needs through the use of software programming languages. It is the application of engineering principles to software development.'
+  },
+  // More questions...
+];
