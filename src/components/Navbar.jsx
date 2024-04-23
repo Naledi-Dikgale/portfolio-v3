c56d49395dd47b38d139a8c11acda81018d26055
naledi-dikgale <naledi22khosi@gmail.com>
2024-04-24 00:52:37 +0200

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
           <span className="font-agbalumo text-2xl">
             <img src="/logo.png" alt="Logo" className="inline-block rounded-full p-1 h-12" />
           </span>
-          <span className={`font-lobster font-bold text-xl ${theme === 'light' ? 'text-primary-dark' : 'text-secondary-light'}`}>NALEDI</span>
+          <span className={`font-lobster font-bold text-xl ${theme === 'light' ? 'text-primary-dark' : 'text-primary'}`}>NALEDI</span>
         </div>
         <div className={`hidden md:flex space-x-4 font-agbalumo font-bold text-xl  ${theme === 'light' ? 'border-primary-light text-primary-dark' : 'border-secondary-light text-secondary-light'}`}>
           <a href="/" className="text-button">Home</a>
@@ -36,17 +36,17 @@ function Navbar() {
         </div>
       </div>
       {showModal && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center md:hidden">
-          <div className="bg-white rounded-lg p-6">
-            <FaTimes onClick={toggleModal} />
-            <div className="flex flex-col space-y-4 mt-4 font-agbalumo">
-              <a href="/" className="text-button">Home</a>
-              <a href="/about" className="text-button">About</a>
-              <a href="/contact" className="text-button">Contact</a>
-            </div>
-          </div>
-        </div>
-      )}
+  <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center md:hidden">
+    <div className="bg-white rounded-lg p-6 w-full h-full flex flex-col justify-center items-center">
+      <FaTimes className="text-4xl font-bold" onClick={toggleModal} />
+      <div className="flex flex-col space-y-4 mt-4 font-agbalumo font-bold text-3xl p-4">
+        <a href="/" className="text-button border-2 border-black p-2">Home</a>
+        <a href="/about" className="text-button border-2 border-black p-2">About</a>
+        <a href="/contact" className="text-button border-2 border-black p-2">Contact</a>
+      </div>
+    </div>
+  </div>
+)}
     </nav>
   );
 }
