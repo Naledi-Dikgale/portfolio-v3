7d1d33ecb1c64e3a10ee3ce42d206af739984cee
naledi-dikgale <naledi22khosi@gmail.com>
2024-04-24 01:44:07 +0200

    update styling

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -13,7 +13,7 @@ function ScrollToTop() {
   };
 
   return (
-    <button onClick={scrollToTop} className={`fixed bottom-4 right-4 px-4 py-2 border-2 rounded ${theme === 'light' ? 'border-secondary text-secondary hover:bg-button hover:text-primary-light' : 'border-secondary-light text-secondary-light hover:bg-button hover:text-primary'}`}>
+    <button onClick={scrollToTop} className={`fixed bottom-4 right-4 px-4 py-2 border-2 rounded ${theme === 'light' ? 'border-button text-button hover:bg-button hover:text-primary' : 'border-secondary-light text-secondary-light hover:bg-button hover:text-primary'}`}>
       <FaArrowUp size={24} />
     </button>
   );
