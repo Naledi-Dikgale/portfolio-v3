54b9a51e49816600a865ead0b342f517508ecb03
naledi-dikgale <naledi22khosi@gmail.com>
2024-04-24 23:58:21 +0200

    Remove Feature

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,17 +1,26 @@
-import React, { useContext } from 'react';
-import { ThemeContext } from '../ThemeContext';
+// import React, { useContext } from 'react';
+// import { ThemeContext } from '../ThemeContext';
 
-function Features() {
-  const { theme } = useContext(ThemeContext);
+// function Features() {
+//   const { theme } = useContext(ThemeContext);
 
-  return (
-    <section className={`p-6 flex flex-col items-center justify-center h-screen ${theme === 'light' ? 'bg-primary text-secondary' : 'bg-secondary text-primary'}`}>
-      <h1 className={`font-agbalumo text-4xl text-center ${theme === 'light' ? 'text-secondary' : 'text-primary'}`}>
-        Featured Content
-      </h1>
-      {/*content */}
-    </section>
-  );
-}
+//   return (
+//     <section className={`p-6 flex flex-col items-center justify-center h-screen ${theme === 'light' ? 'bg-primary text-secondary' : 'bg-secondary text-primary'}`}>
+//       <h1 className={`font-agbalumo text-4xl text-center ${theme === 'light' ? 'text-secondary' : 'text-primary'}`}>
+//         Featured Content
+//       </h1>
+//       <div className="flex flex-col items-center space-y-4 md:space-y-0 md:space-x-4">
+//         <div className="flex justify-center items-center space-x-4">
+//           <img src="Capture (1).png" alt="Capture1" className="w-full max-w-xs" />
+//           <img src="Capture (2).png" alt="Capture2" className="w-full max-w-xs" />
+//         </div>
+//         <div className="flex justify-center items-center space-x-4">
+//           <img src="Capture (3).png" alt="Capture3" className="w-full max-w-xs" />
+//           <img src="Capture (4).png" alt="Capture4" className="w-full max-w-xs" />
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
 
-export default Features;
+// export default Features;
