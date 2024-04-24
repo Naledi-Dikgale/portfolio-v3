275da893e87b5e1c95693d87344cb80b0e1662d8
naledi-dikgale <naledi22khosi@gmail.com>
2024-04-24 23:58:47 +0200

    Add images

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,12 +5,34 @@ import { ThemeContext } from '../ThemeContext';
 const mockData = [
   {
     name: 'John Doe',
-    testimonial: 'Great work!'
+    testimonial: 'Great work!',
+    icon: 'test(3).png'
   },
   {
     name: 'Jane Doe',
-    testimonial: 'Excellent job!'
-  }
+    testimonial: 'Profound expertise and passion!',
+    icon: 'test(1).png'
+  },
+  {
+    name: 'Alice Smith',
+    testimonial: 'Fantastic service!',
+    icon: 'test(2).png'
+  },
+  // {
+  //   name: 'Bob Johnson',
+  //   testimonial: 'Loved the final product!',
+  //   icon: 'test(4).png'
+  // },
+  // {
+  //   name: 'Charlie Brown',
+  //   testimonial: 'Very professional!',
+  //   icon: 'test(2).png'
+  // },
+  // {
+  //   name: 'David Williams',
+  //   testimonial: 'Highly recommend!',
+  //   icon: 'test(2).png'
+  // }
 ];
 
 function Testimonials() {
@@ -27,14 +49,17 @@ function Testimonials() {
 
   return (
     <section className={`p-6 flex flex-col items-center justify-center h-screen ${theme === 'light' ? 'bg-primary text-secondary' : 'bg-secondary text-primary'}`}>
-      <h1 className={`font-agbalumo text-4xl text-center ${theme === 'light' ? 'text-secondary' : 'text-primary'}`}>
+      <h1 className={`font-agbalumo font-bold text-4xl text-center ${theme === 'light' ? 'text-secondary' : 'text-primary'}`}>
         Testimonials
       </h1>
       {data.map((item, index) => (
-        <div key={index} className="text-center mt-4">
-          <h2 className="font-agbalumo text-lg">{item.name}</h2>
-          <p>{item.testimonial}</p>
-        </div>
+        <span key={index} className="text-center mt-4">
+          <div className="flex flex-col items-center">
+            <img src={item.icon} alt={item.name} className="w-12 h-12 rounded-full mx-auto"/>
+            <h2 className="font-agbalumo text-xl">{item.name}</h2>
+          </div>
+          <p className="font-lobster text-lg">{item.testimonial}</p>
+        </span>
       ))}
     </section>
   );
