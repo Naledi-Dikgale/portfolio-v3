865a60d96adba8938fe13df659f0820d558e6f59
naledi-dikgale <naledi22khosi@gmail.com>
2024-04-24 23:59:43 +0200

    Update constants

--- src/constants.js
+++ src/constants.js
@@ -1,27 +1,19 @@
 const FAQ_DATA = [
   {
-    question: 'What is software engineering?',
-    answer: 'Software engineering is the process of analyzing user needs and designing, constructing, and testing end user applications that will satisfy these needs through the use of software programming languages. It is the application of engineering principles to software development.'
+    question: 'What skills do you posess as an individual?',
+    answer: 'I am a highly motivated individual with a passion for learning and a strong work ethic. I have excellent communication skills and am able to work well in a team environment. I am also proficient in a number of programming languages and have experience with a variety of software tools.'
   },
   {
-    question: 'What is a software engineer?',
-    answer: 'A software engineer is a person who applies the principles of software engineering to the design, development, maintenance, testing, and evaluation of computer software.'
+   question: 'What are your strengths and weaknesses?',
+    answer: 'My strengths include my ability to work well under pressure, my strong problem-solving skills, and my attention to detail. My weaknesses include my tendency to be a perfectionist and my difficulty saying no to requests for help.'
   },
   {
-    question: 'What is the difference between a software engineer and a software developer?',
-    answer: 'The terms "software engineer" and "software developer" are often used interchangeably. However, some people make a distinction between the two roles. A software engineer is typically involved in the design and architecture of a software system, while a software developer is more focused on writing code to implement the design.'
+    question: 'Why should we hire you?',
+    answer: 'You should hire me because I am a hard worker who is dedicated to producing high-quality work. I am also a quick learner who is eager to take on new challenges and expand my skill set. I am confident that I would be a valuable addition to your team.'
   },
   {
-    question: 'What are the key skills required to become a software engineer?',
-    answer: 'Some key skills required to become a software engineer include programming skills, problem-solving skills, analytical skills, communication skills, and teamwork skills. Additionally, software engineers should have a good understanding of software development methodologies, software design patterns, and software testing techniques.'
-  },
-  {
-    question: 'What are the different types of software engineers?',
-    answer: 'There are many different types of software engineers, including front-end engineers, back-end engineers, full-stack engineers, mobile engineers, embedded engineers, and quality assurance engineers. Each type of software engineer specializes in a different area of software development.'
-  },
-  {
-    question: 'What is the average salary of a software engineer?',
-    answer: 'The average salary of a software engineer varies depending on factors such as location, experience, and education. According to the U.S. Bureau of Labor Statistics, the median annual wage for software developers was $110,140 in May 2020.'
+   question: 'What do you enjoy doing in your free time?',
+   answer: 'Road trips are my go-to adventure; there is just something magical about the open road.Constantly seeking new destinations to add to my ever-growing travel bucket list!'
   }
 ];
 
