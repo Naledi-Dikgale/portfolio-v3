a635da0ddbe14b53eac00f978664e0a4b1d576f4
naledi-dikgale <naledi22khosi@gmail.com>
2024-04-25 00:00:21 +0200

    Update layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
-import { FaInstagram, FaLinkedin, FaFacebook, FaEnvelope, FaPhone, FaMapPin } from 'react-icons/fa';
+import { FaLinkedin, FaFacebook, FaEnvelope, FaPhone, FaMapPin, FaGithub } from 'react-icons/fa';
+import { RiMediumFill } from "react-icons/ri";
 import { ThemeContext } from '../ThemeContext';
 
 const Contact = () => {
@@ -7,27 +8,27 @@ const Contact = () => {
 
   return (
     <div id="contact" className={`w-screen min-h-screen flex flex-col justify-center items-center pt-24 md:pt-20 bg-cover bg-center ${theme === 'light' ? 'bg-primary text-secondary' : 'bg-secondary text-primary'}`} style={{ backgroundImage: 'url(/book.png)' }}>
-      <h2 className={`text-3xl font-agbalumo font-bold mb-4 ${theme === 'light' ? 'text-secondary' : 'text-primary'}`}>Contact</h2>
-      <div className="flex flex-col md:flex-row justify-between w-full max-w-screen-lg mx-auto bg-white bg-opacity-50 p-4 rounded-lg">
+      <h2 className={`text-3xl font-agbalumo font-bold mb-4 ${theme === 'light' ? 'text-secondary' : 'text-primary'}`}>Contact Me!</h2>
+      <div className="flex flex-col md:flex-row justify-between w-full max-w-screen-lg mx-auto">
         <div className="flex flex-col space-y-4">
-          <a href="https://instagram.com" className="flex items-center space-x-2 hover:text-button font-agbalumo">
-            <FaInstagram size={24} />
-            <span>Instagram</span>
+          <a href="https://github.com" className="flex items-center space-x-2 hover:text-button font-agbalumo">
+            <FaGithub size={24} />
+            <span>Github</span>
           </a>
           <a href="https://linkedin.com" className="flex items-center space-x-2 hover:text-button font-agbalumo">
             <FaLinkedin size={24} />
             <span>LinkedIn</span>
           </a>
-          <a href="https://facebook.com" className="flex items-center space-x-2 hover:text-button font-agbalumo">
-            <FaFacebook size={24} />
-            <span>Facebook</span>
+          <a href="https://medium.com" className="flex items-center space-x-2 hover:text-button font-agbalumo">
+            <RiMediumFill size={24} />
+            <span>Medium</span>
           </a>
         </div>
 
         <div className="flex flex-col space-y-4">
           <div className="flex items-center space-x-2 hover:text-button font-agbalumo">
             <FaEnvelope size={24} />
-            <p>Email: email@audioWise.co.za</p>
+            <p>Email: email@naledi.co.za</p>
           </div>
           <div className="flex items-center space-x-2 hover:text-button font-agbalumo">
             <FaPhone size={24} />
@@ -36,8 +37,7 @@ const Contact = () => {
           <div className="flex items-center space-x-2 font-agbalumo">
             <FaMapPin size={24} className="text-red-700" />
             <p className="hover:text-button">
-              Address: 123 Naledi Street
-              Cape Town, North, 9745, South Africa
+              Location: Cape Town, South Africa
             </p>
           </div>
         </div>
