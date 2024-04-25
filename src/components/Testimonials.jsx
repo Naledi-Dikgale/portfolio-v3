e9b9470051aafe19e47d63a84e0e73e20a9e36e6
naledi-dikgale <naledi22khosi@gmail.com>
2024-04-25 21:26:53 +0200

    Update layout

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from '../ThemeContext';
+import { FaThumbsUp, FaStar } from 'react-icons/fa';
+import { useMediaQuery } from 'react-responsive';
+
 
 // Mock data
 const mockData = [
@@ -38,6 +41,7 @@ const mockData = [
 function Testimonials() {
   const { theme } = useContext(ThemeContext);
   const [data, setData] = useState(mockData);
+  const isSmallScreen = useMediaQuery({ query: '(max-width: 600px)' });
 
   useEffect(() => {
     // Fetch data from LinkedIn API
@@ -48,8 +52,10 @@ function Testimonials() {
   }, []);
 
   return (
-    <section className={`p-6 flex flex-col items-center justify-center h-screen ${theme === 'light' ? 'bg-primary text-secondary' : 'bg-secondary text-primary'}`}>
-      <h1 className={`font-agbalumo font-bold text-4xl text-center ${theme === 'light' ? 'text-secondary' : 'text-primary'}`}>
+    <section className={`p-3 flex flex-col items-center justify-center h-screen ${theme === 'light' ? 'bg-primary text-secondary' : 'bg-secondary text-primary'}`}>
+  <div className="flex flex-col flex-grow items-center justify-between">
+    <div>
+      <h1 className={`font-agbalumo font-bold text-4xl text-center mb-8 ${theme === 'light' ? 'text-secondary' : 'text-primary'}`}>
         Testimonials
       </h1>
       {data.map((item, index) => (
@@ -61,7 +67,15 @@ function Testimonials() {
           <p className="font-lobster text-lg">{item.testimonial}</p>
         </span>
       ))}
-    </section>
+    </div>
+    <div className={`flex ${isSmallScreen ? 'justify-center' : 'justify-end'} mb-4 mr-4`}>
+      <FaThumbsUp className={`text-2xl ${theme === 'light' ? 'text-button' : 'text-button'}`} />
+      {[...Array(5)].map((_, i) => (
+        <FaStar key={i} className={`text-2xl ml-2 ${theme === 'light' ? 'text-button' : 'text-button'}`} />
+      ))}
+    </div>
+  </div>
+</section>
   );
 }
 
