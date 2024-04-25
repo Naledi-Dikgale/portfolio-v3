5ba3946b9f16fe874e924b8aad4e3d6114fbe04e
naledi-dikgale <naledi22khosi@gmail.com>
2024-04-25 21:26:41 +0200

    Update layout

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,7 +9,7 @@ function Intro() {
       <div className="flex flex-row items-center justify-center mt-4 space-x-6">
         <div className="md:w-1/2 md:mt-0 mt-4 px-4 flex flex-col items-center justify-center">
           <h1 className={`text-4xl text-center font-agbalumo font-bold mb-4 ${theme === 'light' ? 'text-secondary' : 'text-primary'}`}>
-            Hello, I'm Naledi
+          <span className="text-button">Hello,</span> I'm Naledi
           </h1>
           <p className={`text-center font-agbalumo font-bold text-xl mb-4 ${theme === 'light' ? 'text-secondary' : 'text-secondary-light'}`}>
           Your one-stop destination for 
@@ -22,7 +22,7 @@ function Intro() {
           <a href="https://naledi.netlify.app" className={`px-4 py-2 border-2 rounded ${theme === 'light' ? 'border-secondary bg-button text-primary hover:bg-button hover:text-primary-light' : 'border-primary bg-button text-primary hover:bg-secondary-light hover:text-primary'}`}>Hire Me</a>
         </div>
         <div className="flex flex-col items-center justify-center relative hidden md:block">
-          <img src="profile0.png" alt="Profile" className="mb-6 w-full max-w-xs" />
+          <img src="profile1.png" alt="Profile" className="mb-6 w-full max-w-xs" />
           <div className={`absolute bottom-0 w-full h-3/4 ${theme === 'light' ? 'bg-primary' : 'bg-secondary'} transform skew-y-45 translate-y-48`}></div>
         </div>
       </div>
