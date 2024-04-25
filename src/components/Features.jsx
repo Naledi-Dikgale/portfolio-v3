7ab8bb4bdb81810eb7b5f0af3b89c2cd24d729a3
naledi-dikgale <naledi22khosi@gmail.com>
2024-04-25 21:26:28 +0200

    Update layout

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -11,12 +11,12 @@
 //       </h1>
 //       <div className="flex flex-col items-center space-y-4 md:space-y-0 md:space-x-4">
 //         <div className="flex justify-center items-center space-x-4">
-//           <img src="Capture (1).png" alt="Capture1" className="w-full max-w-xs" />
-//           <img src="Capture (2).png" alt="Capture2" className="w-full max-w-xs" />
+//           <img src="demo3.png" alt="Capture1" className="w-full max-w-xs" />
+//           <img src="demo1.png" alt="Capture2" className="w-full max-w-xs" />
 //         </div>
 //         <div className="flex justify-center items-center space-x-4">
-//           <img src="Capture (3).png" alt="Capture3" className="w-full max-w-xs" />
-//           <img src="Capture (4).png" alt="Capture4" className="w-full max-w-xs" />
+//           <img src="demo2.png" alt="Capture3" className="w-full max-w-xs" />
+//           {/* <img src="Capture (4).png" alt="Capture4" className="w-full max-w-xs" /> */}
 //         </div>
 //       </div>
 //     </section>
