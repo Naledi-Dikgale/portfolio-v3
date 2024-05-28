51b881503873225ce63f717b953bc2dfa376981d
naledi-dikgale <naledi22khosi@gmail.com>
2024-05-28 20:20:10 +0200

    Update layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,12 @@ function About() {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <section id="about" className={`h-full mx-auto pb-16 pt-24 ${theme === 'light' ? 'bg-primary text-secondary' : 'bg-secondary text-primary'}`}>
+    <section id="about" className={`h-full mx-auto pb-16 pt-24 ${theme === 'light' ? 'bg-primary text-secondary' : 'bg-gradient-to-r from-slate-800 via-slate-850 to-slate-900 text-primary'}`}>
       <h1 className={`font-agbalumo text-3xl font-bold mb-4 text-center ${theme === 'light' ? 'text-secondary' : 'text-primary'}`}>
         About Me
       </h1>
       <div className="w-full h-auto space-y-4 max-w-3xl mx-auto">
-      <p className={`text-center font-agbalumo font-bold text-xl mb-4 ${theme === 'light' ? 'text-secondary' : 'text-secondary-light'}`}>
+      <p className={`text-center font-andika text-xl mb-4 ${theme === 'light' ? 'text-secondary' : 'text-secondary-light'}`}>
             I love to design and code things from scratch.
             I can help you build a product, feature or website.
             Look through some of my work and experience!
@@ -31,14 +31,14 @@ function About() {
           </a>
         </div>
         <div className="flex justify-center space-x-4">
-          <motion.a href="https://docs.google.com/document/d/1wR3kXk9uqbYAIzUEHH7A88pr0xJ_-VsMspL9PcTyViE/edit?usp=sharing" className={`px-4 py-2 border-2 rounded ${theme === 'light' ? 'bg-gradient-to-r from-rose-500 via-pink-400 to-pink-800 text-primary hover:bg-secondary-light hover:text-primary font-bold text-center text-lg' : 'bg-gradient-to-r from-rose-500 via-pink-400 to-pink-800 text-primary hover:bg-secondary-light hover:text-primary font-bold text-center text-lg'}`}
+          <motion.a href="https://docs.google.com/document/d/1wR3kXk9uqbYAIzUEHH7A88pr0xJ_-VsMspL9PcTyViE/edit?usp=sharing" className={`px-4 py-2 border-2 rounded-full ${theme === 'light' ? 'bg-gradient-to-r from-rose-500 via-pink-400 to-pink-800 text-primary hover:bg-secondary-light hover:text-primary font-bold text-center text-lg' : 'bg-gradient-to-r from-rose-500 via-pink-400 to-pink-800 text-primary hover:bg-secondary-light hover:text-primary font-bold text-center text-lg'}`}
           whileHover={{ scale: 1.1, boxShadow: "0px 0px 8px rgb(255, 182, 193)" }}
           whileTap={{ scale: 0.9 }}
           transition={{ type: "spring", stiffness: 300 }}
           >
             Resume
           </motion.a>
-          <motion.a href="https://naledi.netlify.app" className={`px-4 py-2 border-2 rounded ${theme === 'light' ? 'bg-gradient-to-r from-rose-500 via-pink-400 to-pink-800 text-primary hover:bg-secondary-light hover:text-primary font-bold text-center text-lg' : 'bg-gradient-to-r from-rose-500 via-pink-400 to-pink-800 text-primary hover:bg-secondary-light hover:text-primary font-bold text-center text-lg'}`}
+          <motion.a href="https://naledi.netlify.app" className={`px-4 py-2 border-2 rounded-full ${theme === 'light' ? 'bg-gradient-to-r from-rose-500 via-pink-400 to-pink-800 text-primary hover:bg-secondary-light hover:text-primary font-bold text-center text-lg' : 'bg-gradient-to-r from-rose-500 via-pink-400 to-pink-800 text-primary hover:bg-secondary-light hover:text-primary font-bold text-center text-lg'}`}
           whileHover={{ scale: 1.1, boxShadow: "0px 0px 8px rgb(255, 182, 193)" }}
           whileTap={{ scale: 0.9 }}
           transition={{ type: "spring", stiffness: 300 }}
