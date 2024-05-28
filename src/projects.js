61ad1705b96584d5967ce8f8ee8fc1072836681d
naledi-dikgale <naledi22khosi@gmail.com>
2024-05-28 20:17:36 +0200

    Add projects

---
+++ src/projects.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    image: '/Capture (4).png',
+    title: 'Console Hub',
+    description: 'A gaming console rental app built with React, Rails, and Tailwind.',
+    sourceCodeLink: 'https://github.com/user/console-hub',
+    liveLink: 'https://console-hub.com',
+    techStack: ['FaReact', 'SiRails', 'SiTailwindcss']
+  },
+  {
+    image: '/Capture (4).png',
+    title: 'Console Hub',
+    description: 'A gaming console rental app built with React, Rails, and Tailwind.',
+    sourceCodeLink: 'https://github.com/user/console-hub',
+    liveLink: 'https://console-hub.com',
+    techStack: ['FaReact', 'SiRails', 'SiTailwindcss']
+  },
+  {
+    image: '/youtube.png',
+    title: 'YouTube Dummy',
+    description: 'A YouTube clone with minimal functionality aimed at practicing HTML and CSS.',
+    sourceCodeLink: 'https://github.com/Naledi-Dikgale/YouTube-dummy',
+    liveLink: 'https://naledi-dikgale.github.io/YouTube-dummy/',
+    techStack: ['FaReact', 'SiRails', 'SiTailwindcss']
+  },
+  {
+    image: '/Capture (4).png',
+    title: 'Console Hub',
+    description: 'A gaming console rental app built with React, Rails, and Tailwind.',
+    sourceCodeLink: 'https://github.com/user/console-hub',
+    liveLink: 'https://console-hub.com',
+    techStack: ['FaReact', 'SiRails', 'SiTailwindcss']
+  },
+];
