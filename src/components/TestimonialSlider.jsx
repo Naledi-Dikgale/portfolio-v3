a3f5abd924487caf68a39e3908ddca2558ef9c8d
naledi-dikgale <naledi22khosi@gmail.com>
2024-05-28 20:20:57 +0200

    Update layout

--- src/components/TestimonialSlider.jsx
+++ src/components/TestimonialSlider.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import TestimonialCard from '../Cards/TestimonialCard';
-import { TbArrowBigRightLines, TbArrowBigLeftLines } from 'react-icons/tb';
+import { FaForwardFast, FaBackwardFast } from "react-icons/fa6";
 import { ThemeContext } from '../ThemeContext';
 
 const TestimonialSlider = ({ testimonials }) => {
@@ -45,33 +45,37 @@ const TestimonialSlider = ({ testimonials }) => {
   if (!testimonials || testimonials.length === 0) return null;
 
   return (
-    <div className="pt-8 pb-8 w-full flex flex-col items-center">
-      <div className="flex justify-between items-center w-full">
-        <button className={currentIndex === 0 ? 'focus' : ''} onClick={handlePrev}><TbArrowBigLeftLines fill={theme === 'dark' ? '#dbeafe' : '#93c5fd'} size="2em" /></button>
-        {current.map((index, i) => {
-          const { icon: img, name, job, country, testimonial: description } = testimonials[index];
-          return (
-            <div key={i} className={`testimonial-card ${index === currentIndex ? 'slide-in' : 'slide-out'}`}>
-              <TestimonialCard
-                img={img}
-                name={name}
-                job={job}
-                country={country}
-                description={description}
-                theme={theme}
-              />
-            </div>
-          );
-        })}
-        <button className={currentIndex === testimonials.length - 1 ? 'focus' : ''} onClick={handleNext}><TbArrowBigRightLines fill={theme === 'dark' ? '#dbeafe' : '#93c5fd'} size="2em" /></button>
+  <div className="pt-8 pb-8 flex flex-col justify-center items-center">
+    <div className="flex justify-between items-center">
+      <div style={{ flex: 1, display: 'flex', justifyContent: 'center', paddingLeft: '1em' }}>
+        <button className={currentIndex === 0 ? 'focus' : ''} onClick={handlePrev}><FaBackwardFast fill={theme === 'dark' ? '#dbeafe' : '#93c5fd'} size="2em" /></button>
       </div>
-      <div className="pagination mt-4">
-        {testimonials.map((_, index) => (
-          <div key={index} className={`dot ${index === currentIndex ? 'active' : ''}`} />
-        ))}
+      {current.map((index, i) => {
+        const { icon: img, name, job, country, testimonial: description } = testimonials[index];
+        return (
+          <div key={i} className={`testimonial-card ${index === currentIndex ? 'slide-in' : 'slide-out'}`} style={{ flex: 2 }}>
+            <TestimonialCard
+              img={img}
+              name={name}
+              job={job}
+              country={country}
+              description={description}
+              theme={theme}
+            />
+          </div>
+        );
+      })}
+      <div style={{ flex: 1, display: 'flex', justifyContent: 'center', paddingRight: '1em' }}>
+        <button className={currentIndex === testimonials.length - 1 ? 'focus' : ''} onClick={handleNext}><FaForwardFast fill={theme === 'dark' ? '#dbeafe' : '#93c5fd'} size="2em" /></button>
       </div>
     </div>
-  );
+    <div className="pagination mt-4">
+      {testimonials.map((_, index) => (
+        <div key={index} className={`dot ${index === currentIndex ? 'active' : ''}`} />
+      ))}
+    </div>
+  </div>
+);
 };
 
 export default TestimonialSlider;
