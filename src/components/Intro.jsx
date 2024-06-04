56bff9e28cdb4b8a0e58ec3fc1f7d369ecb785f3
naledi-dikgale <naledi22khosi@gmail.com>
2024-06-04 21:47:58 +0200

    Fix overflow

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,7 @@ function Intro() {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <section className={`p-6 flex flex-col items-center justify-center mt-4 ${theme === 'light' ? 'bg-primary text-secondary' : 'bg-gradient-to-r from-slate-800 via-slate-850 to-slate-900 text-primary'}`}>
+    <section className={`h-screen p-6 flex flex-col items-center justify-center ${theme === 'light' ? 'bg-primary text-secondary' : 'bg-gradient-to-r from-slate-800 via-slate-850 to-slate-900 text-primary'}`}>
       <div className="flex flex-row items-center justify-center mt-4 space-x-6">
         <div className="md:w-1/2 md:mt-0 mt-4 px-4 flex flex-col md:pl-8 ">
           <h1 className={`text-4xl font-agbalumo font-bold mb-4 ${theme === 'light' ? 'text-secondary' : 'text-primary'}`}>
@@ -20,17 +20,19 @@ function Intro() {
               awe-inspiring web experiences that leave a lasting
               impression. Let&apos;s turn your vision into a digital masterpiece!
           </p>
-            <div className="md:w-1/2">
-              <motion.a 
+            <motion.div className=""
+            whileHover={{ scale: 1.1, boxShadow: "0px 0px 8px rgb(255, 182, 193)" }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ type: "spring", stiffness: 300 }}
+            >
+              <a 
                 href="https://naledi.netlify.app" 
                 className={`px-4 py-2 p-2 border-2 rounded-full font-bold text-lg ${theme === 'light' ? 'bg-gradient-to-r from-rose-500 via-pink-400 to-pink-800 text-primary hover:bg-secondary-light hover:text-primary font-bold text-center text-lg' : 'bg-gradient-to-r from-rose-500 via-pink-400 to-pink-800 text-primary hover:bg-secondary-light hover:text-primary font-bold text-center'}`}
-                whileHover={{ scale: 1.1, boxShadow: "0px 0px 8px rgb(255, 182, 193)" }}
-                whileTap={{ scale: 0.9 }}
-                transition={{ type: "spring", stiffness: 300 }}
+                
               >
                 Hire Me
-              </motion.a>
-            </div>
+              </a>
+            </motion.div>
         </div>
          <div className="flex flex-col items-center justify-center relative hidden md:block rounded-full border-1 overflow-hidden transform scale-50 ">
           <div className="flex items-center justify-center diagonal-lines">
