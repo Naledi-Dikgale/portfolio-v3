2950421f12980a7532d4bd001556cce215940212
naledi-dikgale <naledi22khosi@gmail.com>
2024-06-06 14:20:42 +0200

    Fix bugs

--- src/components/TestimonialSlider.jsx
+++ src/components/TestimonialSlider.jsx
@@ -45,40 +45,39 @@ const TestimonialSlider = ({ testimonials }) => {
   if (!testimonials || testimonials.length === 0) return null;
 
   return (
-    <div className="pt-8 pb-8 flex flex-col justify-center items-center">
-      <div className="flex justify-between items-center">
-  <div className="flex-1 flex justify-center pl-4 md:pl-1">
-    <button className={currentIndex === 0 ? 'focus' : ''} onClick={handlePrev}><MdArrowBackIos fill={theme === 'dark' ? '#dbeafe' : '#94a3b8'} size="2em" /></button>
-  </div>
-  <div className="flex-1 flex justify-center">
-    {current.map((index, i) => {
-      const { icon: img, name, job, country, testimonial: description } = testimonials[index];
-      return (
-        <div key={i} className={`testimonial-card ${index === currentIndex ? 'slide-in' : 'slide-out'}`}>
-          <TestimonialCard
-            img={img}
-            name={name}
-            job={job}
-            country={country}
-            description={description}
-            theme={theme}
-          />
-        </div>
-      );
-    })}
-  </div>
-  <div className="flex-1 flex justify-center pr-4 md:pr-1">
-    <button className={currentIndex === testimonials.length - 1 ? 'focus' : ''} onClick={handleNext}><MdArrowForwardIos fill={theme === 'dark' ? '#dbeafe' : '#94a3b8'} size="2em" /></button>
-  </div>
-</div>
-     
-      <div className="pagination mt-4">
-        {testimonials.map((_, index) => (
-          <div key={index} className={`dot ${index === currentIndex ? 'active' : ''}`} />
-        ))}
+  <div className="pt-8 pb-8 flex flex-col justify-center items-center">
+    <div className="flex justify-between items-center w-full">
+      <div className="flex justify-start pl-2 md:pl-1">
+        <button className={currentIndex === 0 ? 'focus' : ''} onClick={handlePrev}><MdArrowBackIos fill={theme === 'dark' ? '#dbeafe' : '#94a3b8'} size="2em" /></button>
+      </div>
+      <div className="flex-1 flex justify-center">
+        {current.map((index, i) => {
+          const { icon: img, name, job, country, testimonial: description } = testimonials[index];
+          return (
+            <div key={i} className={`testimonial-card `}>
+              <TestimonialCard
+                img={img}
+                name={name}
+                job={job}
+                country={country}
+                description={description}
+                theme={theme}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex justify-end pr-2 md:pr-1">
+        <button className={currentIndex === testimonials.length - 1 ? 'focus' : ''} onClick={handleNext}><MdArrowForwardIos fill={theme === 'dark' ? '#dbeafe' : '#94a3b8'} size="2em" /></button>
       </div>
     </div>
-  );
+    <div className="pagination mt-4">
+      {testimonials.map((_, index) => (
+        <div key={index} className={`dot ${index === currentIndex ? 'active' : ''}`} />
+      ))}
+    </div>
+  </div>
+);
 };
 
 export default TestimonialSlider;
